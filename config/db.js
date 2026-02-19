cf98e6552df775ac53e202e3d3d06c519e9e26e7
jaysuthar064 <jayantisuthar094@gmail.com>
2026-02-19 19:20:18 +0530

    fix bugs

--- config/db.js
+++ config/db.js
@@ -1,14 +1,12 @@
 import pg from "pg";
-import dotenv from "dotenv";
-dotenv.config();
-const {Pool}=pg;
 
-const db = new Pool({
-    user:process.env.DB_USER,
-    host:process.env.DB_HOST,
-    database:process.env.DB_DATABASE,
-    password:process.env.DB_PASSWORD,
-    port:process.env.DB_PORT,
+const { Pool } = pg;
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
-export default db;
+export default pool;
