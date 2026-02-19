3a7afd485ecbe0cf4cd048212ec8b9c781f78f8a
jaysuthar064 <jayantisuthar094@gmail.com>
2026-02-19 22:04:12 +0530

    add footer

--- config/db.js
+++ config/db.js
@@ -1,12 +1,25 @@
 import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const { Pool } = pg;
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
+const isProduction = process.env.DATABASE_URL;
 
-export default pool;
+const db = new Pool(
+  isProduction
+    ? {
+        connectionString: process.env.DATABASE_URL,
+        ssl: { rejectUnauthorized: false },
+      }
+    : {
+        user: process.env.DB_USER,
+        host: process.env.DB_HOST,
+        database: process.env.DB_DATABASE,
+        password: process.env.DB_PASSWORD,
+        port: process.env.DB_PORT,
+      }
+);
+
+export default db;
